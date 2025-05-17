66333a8f67bb0a0432384061332681c6fc0c3ee2
AndrePereira03 <andre.pereira@estudante.ufjf.br>
2025-05-16 22:48:07 -0300

    exercicio 1171

---
+++ BeeCrowd/ex1171.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    map<int, int> ocorrencia;
+    for(int i = 0; i < n; i++)
+    {
+        int val;
+        cin >> val;
+        ocorrencia[val]++;
+    }
+    for(const pair<const int, int>& par :ocorrencia)
+    cout << par.first << " aparece " << par.second << " vez(es)" << endl;
+    return 0;
+}

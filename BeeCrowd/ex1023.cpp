de7547141a28b17d7b7e36a9cebcd71e3109edf6
AndrePereira03 <andre.pereira@estudante.ufjf.br>
2025-05-18 17:55:43 -0300

    resolvendo ex1023

--- BeeCrowd/ex1023.cpp
+++ BeeCrowd/ex1023.cpp
@@ -1,37 +1,45 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int main()
-{
-    /*
-        quantidade de imoveis
-        par: quantidade de moradores imovel consumo total de agua
-        final da entrada é representado por 0 
-    */
-    int qtdImoveis;
-    int qtdMoradores;
-    int consumoTotal;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
-    cin >> qtdImoveis;
-    while (qtdImoveis != 0)
-    {
-        vector<tuple<int, int>> tupla;
-        for(int i = 1; i < qtdImoveis + 1; i++)
-        {
-            cin >> qtdMoradores >> consumoTotal;
-            tupla[i] = {qtdMoradores, consumoTotal};
+    int N, caso = 1;
+    while ( (cin >> N) && N != 0 ) {
+        if (caso > 1) 
+            cout << "\n";
+
+        vector<int> consumos(201, 0);
+        long long totalPessoas = 0;
+        long long totalConsumo = 0;
+
+        for (int i = 0; i < N; i++) {
+            int X, Y;
+            cin >> X >> Y;
+            totalPessoas += X;
+            totalConsumo  += Y;
+            consumos[Y / X] += X;
         }
 
-        for(int i = 1; i < qtdImoveis + 1; i++)
-        {
-            cout << "Cidade# " << i << ":" << '\n';
-            cout << get<1>(tupla[i])
+        cout << "Cidade# " << caso++ << ":\n";
+        bool primeiro = true;
+        for (int m = 0; m <= 200; m++) {
+            if (consumos[m] > 0) {
+                if (!primeiro) 
+                    cout << " ";
+                cout << consumos[m] << "-" << m;
+                primeiro = false;
+            }
         }
-        
-        cin >> qtdImoveis;
+        cout << "\n";
+
+        double media = floor((100.0 * totalConsumo) / totalPessoas) / 100.0;
+        cout << "Consumo medio: "
+             << fixed << setprecision(2)
+             << media << " m3.\n";
     }
-
-
 
     return 0;
 }

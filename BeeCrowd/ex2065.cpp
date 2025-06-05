bcc2f7b6c9454883ed90f75480269108508698ce
AndrePereira03 <andre.pereira@estudante.ufjf.br>
2025-06-05 12:52:13 -0300

    ex2065

---
+++ BeeCrowd/ex2065.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include <queue>
+
+using namespace std;
+
+
+int main()
+{
+    int n; //funcionarios
+    int m; //clientes
+    queue<int> filaItens;
+    int tempoTotal = 0;
+    cin >> n >> m;
+    vector <int> c(100); //c_j representa a qtd do item j na cesta c
+    vector <int> v(n);//tempo gasto pelo funcionario i pro-
+    //cessar item do cliente
+    for(int i = 0; i < n; i++) cin >> v[i];//tempo gasto funcionario i
+    for(int i = 0; i < m; i++) cin >> filaItens.push();
+    // for(int i = 0; i < m; i++)
+    // {
+       
+    // }
+    cout << filaItens;
+   
+    return 0;
+}

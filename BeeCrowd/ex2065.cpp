bf74d71935575185be874920285e6c96732fb6c3
AndrePereira03 <andre.pereira@estudante.ufjf.br>
2025-06-05 13:08:03 -0300

    adding some features

--- BeeCrowd/ex2065.cpp
+++ BeeCrowd/ex2065.cpp
@@ -1,26 +1,27 @@
 #include <bits/stdc++.h>
-#include <queue>
 
 using namespace std;
 
+//
+typedef pair<long long, int> Funcionario;
 
 int main()
 {
     int n; //funcionarios
     int m; //clientes
-    queue<int> filaItens;
     int tempoTotal = 0;
     cin >> n >> m;
-    vector <int> c(100); //c_j representa a qtd do item j na cesta c
+    vector <int> c(m); //c_j representa a qtd do item j na cesta c
     vector <int> v(n);//tempo gasto pelo funcionario i pro-
-    //cessar item do cliente
+    
     for(int i = 0; i < n; i++) cin >> v[i];//tempo gasto funcionario i
-    for(int i = 0; i < m; i++) cin >> filaItens.push();
-    // for(int i = 0; i < m; i++)
-    // {
-       
-    // }
-    cout << filaItens;
-   
+    for(int i = 0; i < m; i++) cin >> c[i];
+    priority_queue<Funcionario, vector<Funcionario>, greater<Funcionario>> fila;
+    for(int i = 0; i < n; i++) fila.push({0, i});
+    for(int i = 0; i < m; ++i)
+    {
+        auto [livre, idx] = fila.top(); fila.pop();
+        int tempo atend
+    }
     return 0;
 }
